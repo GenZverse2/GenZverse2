0ae76014cb56f9c7103f3a5b50bcce3e9ff198e4
GenZverse2 <gdayoota@gmail.com>
2025-07-23 19:55:07 +0530

    Initial commit for GenZVerse

---
+++ script.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const button = document.querySelector("button");
+
+  button.addEventListener("click", function () {
+    alert("🚀 GenZVerse drop coming soon! Stay ready for the heat 🔥");
+  });
+});
